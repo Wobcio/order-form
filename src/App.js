a6543749376bf43443a739b825af9177461808b2
Igor Batko <igor.batko.batko@gmail.com>
2022-06-05 20:47:53 +0200

    Add theme

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
+import * as React from 'react';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme({
+  status: {
+    danger: '#e53e3e',
+  },
+  palette: {
+    primary: {
+      main: '#f19409',
+      darker: '#ba6d02',
+    },
+    neutral: {
+      main: '#64748B',
+      contrastText: '#fff',
+    },
+  },
+});
 
 function App() {
+
+  
   return (
-    <div></div>
+    <ThemeProvider theme={theme}>
+    </ThemeProvider>
   );
 }
 
